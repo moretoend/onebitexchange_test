8d02aed13ed836d74edb594fbb313c5fdb266044
Daniel Moreto <dfmoreto@gmail.com>
2020-04-23 14:01:17 -0300

    Add actions for :index and :convert

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,5 +3,7 @@ class ExchangesController < ApplicationController
   end
 
   def convert
+    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).call
+    render json: {"value": value}
   end
 end
